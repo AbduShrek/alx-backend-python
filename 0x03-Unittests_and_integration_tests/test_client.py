ac9a968959db4eca12258070d1275bd47dc8a3b0
Abdelrahman Ateya <abdu.shrek.at@gmail.com>
2025-09-22 17:38:37 +0300

    Update test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -10,21 +10,22 @@ from client import GithubOrgClient
 class TestGithubOrgClient(unittest.TestCase):
     """Tests for GithubOrgClient.org property."""
 
+    @patch("client.get_json")
     @parameterized.expand([
         ("google",),
         ("abc",),
     ])
-    @patch("client.get_json")
     def test_org(self, org_name, mock_get_json):
-        """Ensure .org calls get_json once with the right URL and returns it."""
-        expected = {"org": org_name}
-        mock_get_json.return_value = expected
+        """
+        Ensure .org calls get_json once with the right URL and returns
+        the mocked payload.
+        """
+        expected_payload = {"org": org_name}
+        mock_get_json.return_value = expected_payload
 
         client = GithubOrgClient(org_name)
-        self.assertEqual(
-            client.org,
-            expected
-        )
+        self.assertEqual(client.org, expected_payload)
+
         mock_get_json.assert_called_once_with(
             GithubOrgClient.ORG_URL.format(org=org_name)
         )
